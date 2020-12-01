6821918e0b8f403c3e36f6b2e195c7de0ba4f136
papilo <reoverduguez@gmail.com>
2020-12-01 12:41:35 -0500

    commiting latest from branch

--- app/src/main/java/com/example/corsiblocktappingtask/TaskActivity.kt
+++ app/src/main/java/com/example/corsiblocktappingtask/TaskActivity.kt
@@ -1,6 +1,8 @@
 package com.example.corsiblocktappingtask
 
 import android.app.Activity
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -8,6 +10,7 @@ import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import java.util.*
+import kotlin.collections.ArrayList
 
 class TaskActivity: Activity() {
 
@@ -19,6 +22,7 @@ class TaskActivity: Activity() {
     lateinit var doneBtn: Button
     lateinit var tableLayout: TableLayout
     lateinit var random: Random
+    lateinit var boxes: ArrayList<TextView>
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,18 +33,30 @@ class TaskActivity: Activity() {
         doneBtn = findViewById(R.id.done)
         tableLayout = findViewById(R.id.tablelayout)
         random = Random()
+        boxes = ArrayList<TextView>()
 
         start()
     }
 
     // function designed to start the sequence
     private fun start() {
+        // initialize 9 boxes
         for (row_index in 0 until tableLayout.childCount) {
             val tableRow: TableRow = tableLayout.getChildAt(row_index) as TableRow
             for (box_index in 0 until tableRow.childCount) {
                 val box: TextView = tableRow.getChildAt(box_index) as TextView
-                Log.i(TAG, box.tag.toString())
+                boxes.add(box)
             }
+        }
+
+        // create random sequence that user must replicate
+        for (seq in 1..sequence) {
+            val i: Int = random.nextInt(8)
+            val colorDrawables: Array<ColorDrawable> =
+                arrayOf(ColorDrawable(resources.getColor(R.color.colorSecondaryDark)), ColorDrawable(resources.getColor(R.color.colorPrimaryDark)))
+            val transitionDrawable: TransitionDrawable = TransitionDrawable(colorDrawables)
+            boxes[i].background = transitionDrawable
+            transitionDrawable.startTransition(1000)
         }
     }
 
